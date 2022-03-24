ea59681285e179424d1b9d1b753de6521a8de2e8
HawkDiego <giraldito99@gmail.com>
2022-03-24 18:39:13 -0500

    Eliminar nodemon

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
+
 let base = path.join(__dirname);
 
-app.get('/', (req, res) => {
-    res.sendFile(base + '/views/home.html')
+app.get("/", (req, res) => {
+  res.sendFile(base + "/views/home.html");
 });
 
-app.get('/login', (req, res) => {
-  res.sendFile(base + '/views/login.html')
+app.get("/login", (req, res) => {
+  res.sendFile(base + "/views/login.html");
 });
 
-app.get('/signup', (req, res) => {
-  res.sendFile(base + '/views/signup.html')
+app.get("/signup", (req, res) => {
+  res.sendFile(base + "/views/signup.html");
 });
 
-
-
-
-
-
-
-
-
-app.listen(3000, () => {
-  console.log("Server ON");
-});
+app.listen(PORT, () => console.log("running on port 3000"));
